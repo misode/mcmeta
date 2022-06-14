084ea316e4190b5631625b0cd5f2598d02326709
Misode <misoloo64@gmail.com>
2022-06-14 17:40:05 +0200

    Close #5 Add license header to main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# MIT License
+# 
+# Copyright (c) 2022 Misode
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import click
 import requests
 import requests.auth
@@ -70,10 +92,10 @@ def main(version: str | None, file, reset: bool, fetch: bool, undo: str | None, 
 	start_date = versions[process_versions[0]]['releaseTime'] if process_versions else None
 	init_exports(start_date, reset, fetch, undo, export, branch)
 
-	try:
-		os.remove('versions.json')
-	except OSError:
-		pass
+	# try:
+	# 	os.remove('versions.json')
+	# except OSError:
+	# 	pass
 
 	if process_versions:
 		click.echo(f'🚧 Processing versions: {", ".join(process_versions)}')
