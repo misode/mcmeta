9a515cf3547c40bf12fac17174f51a41711de62c
actions-user <actions@github.com>
2026-08-04 11:49:07 +0000

    🚀 Update assets-tiny for 26.3-snapshot-7

--- assets/minecraft/shaders/include/oit_depth_sample.glsl
+++ assets/minecraft/shaders/include/oit_depth_sample.glsl
@@ -1,7 +1,7 @@
 uniform sampler2D DepthBoundsSampler;
 
 const float OIT_HIGH_PRECISION_DEPTH_THRESHOLD = 10.0;
-const int OIT_LOW_PRECISION_COEFF_COUNT = 2;
+const int OIT_LOW_PRECISION_DEPTH_BINS = 2;
 
 float normalizeDepth(float fragmentDeviceDepth) {
     vec4 depthBoundsSample = texelFetch(DepthBoundsSampler, ivec2(gl_FragCoord.xy), 0);
@@ -13,17 +13,14 @@ float normalizeDepth(float fragmentDeviceDepth) {
     float range = max(furthestBoundLinearDepth - closestBoundLinearDepth, 0.0001);
     float depthWithinBounds = clamp(fragmentLinearDepth - closestBoundLinearDepth, 0.0, range);
 
-    const float depthFractionWithHighPrecision = float(COEFF_COUNT - OIT_LOW_PRECISION_COEFF_COUNT) / float(COEFF_COUNT);
+    const float depthFractionWithHighPrecision = float(OIT_NUMBER_OF_DEPTH_BINS - OIT_LOW_PRECISION_DEPTH_BINS) / float(OIT_NUMBER_OF_DEPTH_BINS);
     float depthWithHighPrecision = depthFractionWithHighPrecision * range;
 
-    float mappedDepth;
     if (depthWithHighPrecision <= OIT_HIGH_PRECISION_DEPTH_THRESHOLD) {
-        mappedDepth = depthWithinBounds / range;
+        return depthWithinBounds / range;
     } else if (depthWithinBounds <= OIT_HIGH_PRECISION_DEPTH_THRESHOLD) {
-        mappedDepth = (depthWithinBounds / OIT_HIGH_PRECISION_DEPTH_THRESHOLD) * depthFractionWithHighPrecision;
+        return (depthWithinBounds / OIT_HIGH_PRECISION_DEPTH_THRESHOLD) * depthFractionWithHighPrecision;
     } else {
-        mappedDepth = depthFractionWithHighPrecision + ((depthWithinBounds - OIT_HIGH_PRECISION_DEPTH_THRESHOLD) / (range - OIT_HIGH_PRECISION_DEPTH_THRESHOLD)) * (1.0 - depthFractionWithHighPrecision);
+        return depthFractionWithHighPrecision + ((depthWithinBounds - OIT_HIGH_PRECISION_DEPTH_THRESHOLD) / (range - OIT_HIGH_PRECISION_DEPTH_THRESHOLD)) * (1.0 - depthFractionWithHighPrecision);
     }
-
-    return mappedDepth * (1.0 - 1.0 / COEFF_COUNT);
 }
--- assets/minecraft/shaders/include/oit_sample.glsl
+++ assets/minecraft/shaders/include/oit_sample.glsl
@@ -1,71 +1,79 @@
 uniform sampler2D Coeff0;
-#if COEFF_COUNT > 4
+#if OIT_COEFF_COUNT > 4
 uniform sampler2D Coeff1;
 #endif
-#if COEFF_COUNT > 8
+#if OIT_COEFF_COUNT > 8
 uniform sampler2D Coeff2;
 uniform sampler2D Coeff3;
 #endif
 
-float evaluateWaveletsCorrected(float coefficients[COEFF_COUNT], float depth, float currentAbsorbance) {
-    float scaleCoeff = coefficients[COEFF_COUNT - 1];
-    if (scaleCoeff == 0) {
+float sampleAbsorbance(float coefficients[OIT_COEFF_COUNT], float originalDepth, float currentAbsorbance) {
+    float averageAbsorbance = coefficients[0];
+    if (averageAbsorbance == 0) {
         return 0.0;
     }
-    float scaleCoeffAddend = (currentAbsorbance * -depth) + currentAbsorbance;
-    scaleCoeff -= scaleCoeffAddend;
+    float currentAverageAbsorbanceContribution = currentAbsorbance * (1 - originalDepth);
+    averageAbsorbance -= currentAverageAbsorbanceContribution;
 
-    depth *= float(COEFF_COUNT - 1) / COEFF_COUNT;
+    float depthMeasuredInBins = originalDepth * (OIT_NUMBER_OF_DEPTH_BINS - 1);
 
-    float coeffDepth = depth * COEFF_COUNT;
-    int indexB = clamp(int(floor(coeffDepth)), 0, COEFF_COUNT - 1);
-    bool sampleA = indexB >= 1;
-    int indexA = sampleA ? (indexB - 1) : indexB;
+    float depth = depthMeasuredInBins / OIT_NUMBER_OF_DEPTH_BINS;
 
-    indexB += COEFF_COUNT - 1;
-    indexA += COEFF_COUNT - 1;
+    int treeIndexB = clamp(int(floor(depthMeasuredInBins)), 0, OIT_NUMBER_OF_DEPTH_BINS - 1);
+    bool shouldSampleA = treeIndexB >= 1;
+    int treeIndexA = shouldSampleA ? (treeIndexB - 1) : treeIndexB;
 
-    float b = scaleCoeff;
-    float a = sampleA ? scaleCoeff : 0;
+    treeIndexB += OIT_COEFF_COUNT;
+    treeIndexA += OIT_COEFF_COUNT;
 
-    for (int i = 0; i < WAVELET_RANK + 1; i++) {
-        int power = WAVELET_RANK - i;
+    // B is the sample at the depth bin that the fragment is in, and A is the sample at the depth bin just before it.
 
-        int newIndexB = (indexB - 1) >> 1;
-        int waveletSignB = ((indexB & 1) << 1) - 1;
-        float coeffB = coefficients[newIndexB];
+    float sampleB = averageAbsorbance;
+    float sampleA = shouldSampleA ? averageAbsorbance : 0;
 
-        float waveletPhaseB = ((indexB + 1) & 1) * exp2(-power);
-        float k = float((newIndexB + 1) & ((1 << power) - 1));
-        float addend = ((depth - exp2(-power) * k) * waveletSignB + waveletPhaseB) * exp2(power * 0.5) * currentAbsorbance;
-        coeffB -= addend;
+    for (int waveletLevel = 0; waveletLevel <= OIT_WAVELET_RANK; waveletLevel++) {
+        int power = OIT_WAVELET_RANK - waveletLevel;
+        float waveletWidth = exp2(-power);
+        float waveletHeightScale = exp2(power * 0.5);
 
-        b -= exp2(power * 0.5) * coeffB * waveletSignB;
-        indexB = newIndexB;
+        int parentIndexB = treeIndexB >> 1;
+        int isRightHalfB = treeIndexB & 1;
+        int waveletSignB = 1 - (isRightHalfB << 1);
 
-        if (sampleA) {
-            int newIndexA = (indexA - 1) >> 1;
-            int waveletSignA = ((indexA & 1) << 1) - 1;
-            float coeffA = (newIndexA == newIndexB) ? coeffB : coefficients[newIndexA];
-            a -= exp2(power * 0.5) * coeffA * waveletSignA;
-            indexA = newIndexA;
+        float indexAtParentLevel = float(parentIndexB & ((1 << power) - 1));
+        float depthOffsetRelativeToWavelet = depth - waveletWidth * indexAtParentLevel;
+        float currentDifferenceCoefficient = (isRightHalfB * waveletWidth + waveletSignB * depthOffsetRelativeToWavelet) * waveletHeightScale * currentAbsorbance;
+
+        float differenceCoefficientB = coefficients[parentIndexB];
+        differenceCoefficientB -= currentDifferenceCoefficient;
+
+        sampleB -= waveletHeightScale * differenceCoefficientB * waveletSignB;
+        treeIndexB = parentIndexB;
+
+        if (shouldSampleA) {
+            int parentIndexA = treeIndexA >> 1;
+            float differenceCoefficientA = (parentIndexA == parentIndexB) ? differenceCoefficientB : coefficients[parentIndexA];
+            int isRightHalfA = treeIndexA & 1;
+            int waveletSignA = 1 - (isRightHalfA << 1);
+            sampleA -= waveletHeightScale * differenceCoefficientA * waveletSignA;
+            treeIndexA = parentIndexA;
         }
     }
 
-    float t = coeffDepth >= COEFF_COUNT ? 1.0 : fract(coeffDepth);
+    float lerpAlpha = depthMeasuredInBins >= OIT_NUMBER_OF_DEPTH_BINS ? 1.0 : fract(depthMeasuredInBins);
 
-    return mix(a, b, t);
+    return mix(sampleA, sampleB, lerpAlpha);
 }
 
-float sampleTransmittance(ivec2 pos, float depth, float currentAbsorbance) {
-    float coefficients[COEFF_COUNT];
-    const int targetCount = COEFF_COUNT / 4;
+float sampleTransmittance(ivec2 pos, float depth, float currentTransmittance) {
+    float coefficients[OIT_COEFF_COUNT];
+    const int targetCount = OIT_COEFF_COUNT / 4;
     vec4 coeffSamples[targetCount];
     coeffSamples[0] = texelFetch(Coeff0, pos, 0);
-    #if COEFF_COUNT > 4
+    #if OIT_COEFF_COUNT > 4
     coeffSamples[1] = texelFetch(Coeff1, pos, 0);
     #endif
-    #if COEFF_COUNT > 8
+    #if OIT_COEFF_COUNT > 8
     coeffSamples[2] = texelFetch(Coeff2, pos, 0);
     coeffSamples[3] = texelFetch(Coeff3, pos, 0);
     #endif
@@ -74,24 +82,22 @@ float sampleTransmittance(ivec2 pos, float depth, float currentAbsorbance) {
             coefficients[i * 4 + j] = coeffSamples[i][j];
         }
     }
-    return clamp(exp(-evaluateWaveletsCorrected(coefficients, depth, currentAbsorbance)), 0.0001, 1.0);
+    return toTransmittance(sampleAbsorbance(coefficients, depth, toAbsorbance(currentTransmittance)));
 }
 
 #ifdef OIT_ACCUMULATE
 vec4 sampleColorForAccumulation(vec4 color) {
     #ifdef OIT_ADDITIVE
     float transmittance = 1.0;
-    float absorbance = 0.0;
     float accumAlpha = 0.0;
     #else
     float transmittance = 1.0 - color.a;
-    float absorbance = toAbsorbance(transmittance);
     float accumAlpha = color.a;
     #endif
     #ifdef RENDERPEARL_EXPLICIT_DEPTH_INVARIANCE
     gl_FragDepth = gl_FragCoord.z;
     #endif
-    float sampledTransmittance = sampleTransmittance(ivec2(gl_FragCoord.xy), normalizeDepth(gl_FragCoord.z), absorbance);
+    float sampledTransmittance = sampleTransmittance(ivec2(gl_FragCoord.xy), normalizeDepth(gl_FragCoord.z), transmittance);
     return vec4(color.rgb * color.a, accumAlpha) * sampledTransmittance;
 }
 #endif
